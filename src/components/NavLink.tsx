867c589e4ca607400e1271a66177512acbe2ecb4
Timotius Sitorus <timsitorus1@gmail.com>
2022-11-12 14:29:56 +0700

    Add end to navlinks

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -6,7 +6,7 @@ interface NavButtonProps {
 }
 export default function NavLink(props: NavButtonProps) {
   return (
-    <A class="p-4" activeClass="text-green-500" href={props.href}>
+    <A class="p-4" activeClass="text-green-500" end={true} href={props.href}>
       {props.name}
     </A>
   );
